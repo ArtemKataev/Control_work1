619ed021bf62d0aa2ed816987c05b64834dd3319
ArtemKataev <tipa.atlet@mail.ru>
2023-06-05 01:46:10 +0300

    solved task

--- List_1/Program.cs
+++ List_1/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿int size = Convert.ToInt32(Console.ReadLine());
+string [] array = new string [size];
+for (int i = 0; i < size; i++)
+{
+  string number = Convert.ToString(Console.ReadLine());
+  array[i]= number;
+}
+
+
+string[] arrayF = new string[size];
+int lenght = 3;
+int position = 0;
+for (int j = 0; j < size; j++)
+{
+  if(array[j].Length<=lenght)
+  {
+    arrayF[position]= array[j];
+    position++;
+  }
+}
+
+System.Console.WriteLine();
+
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arrayF[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
